344532b8f38c2f1e67af4f198e023d65c82a3623
B. Kerem KURT <kkurt@sabanciuniv.edu>
2021-11-26 09:57:24 +0300

    category page retrieved data

--- lib/app.dart
+++ lib/app.dart
@@ -5,6 +5,7 @@ import 'models/app_state.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'home_screen.dart';
 import 'leaderboard/leaderboard_screen.dart';
+import 'category/category_screen.dart';
 import 'store/store_screen.dart';
 import './login-signup/login_screen.dart';
 import 'package:redux/redux.dart';
@@ -27,6 +28,7 @@ class ReduxApp extends StatelessWidget {
           '/questions': (context) => QuestionsScreen(),
           '/store': (context) => StoreScreen(),
           '/leaderboard': (context) => LeaderboardScreen(),
+          '/category': (context) => CategoryScreen(),
           '/landing': (context) => LandingPage(),
         },
       ),
---
+++ lib/category/category_screen.dart
@@ -0,0 +1,97 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class CategoryScreen extends StatefulWidget {
+  const CategoryScreen({Key key}) : super(key: key);
+
+  @override
+  _CategoryScreenState createState() => _CategoryScreenState();
+}
+
+final firestore = FirebaseFirestore.instance;
+var lastCategory = [];
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  void categoryListMaker(AsyncSnapshot snapshot) {
+    return snapshot.data.docs.map<Widget>((document) {
+      if (!lastCategory.contains(document.get('category'))) {
+        lastCategory.add(document.get('category'));
+        //print("lastcategory ${lastCategory}");
+        //print("last ${lastCategory.last}");
+      }
+    }).toList();
+  }
+
+  List<Widget> getList() {
+    List<Widget> categories = lastCategory
+        .map((e) => Column(children: <Widget>[
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                elevation: 5,
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Spacer(flex: 1),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [Text(e.toString())],
+                        ),
+                        Spacer(flex: 5),
+                        OutlinedButton(
+                            onPressed: () {
+                              //Navigator.of(context).pushNamed('/');
+                              print("clicked ${e.toString()}");
+                            },
+                            child: Text('PLAY',
+                                style: TextStyle(
+                                  color: Color(0xFF80D8FF),
+                                ))),
+                        Spacer(flex: 1),
+                      ],
+                    )),
+              )
+            ]))
+        .toList();
+    return categories;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'CATEGORY',
+          ),
+          backgroundColor: Color(0xFF80D8FF),
+          centerTitle: true,
+          elevation: 0.0,
+          leading: Container(),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/');
+                },
+                icon: Icon(Icons.home)),
+          ],
+        ),
+        body: Column(children: [
+          StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection("questions")
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.data == null)
+                  return Center(child: CircularProgressIndicator());
+                else {
+                  categoryListMaker(snapshot);
+                  return Expanded(
+                      child: ListView(
+                    children: getList(),
+                  ));
+                }
+              }),
+        ]));
+  }
+}
