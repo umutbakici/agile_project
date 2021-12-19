7ddf55819d9f162a8f425e89d1dc67e9eba7639b
B. Kerem KURT <kkurt@sabanciuniv.edu>
2021-12-19 19:56:25 +0300

    reengineered code at store page

--- lib/store/store_screen.dart
+++ lib/store/store_screen.dart
@@ -1,6 +1,4 @@
 import 'package:agile_project/models/user.dart';
-import 'package:agile_project/store/store_listview.dart';
-import 'package:agile_project/store/store_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:agile_project/models/app_state.dart';
@@ -10,33 +8,185 @@ class StoreScreen extends StatefulWidget {
   _StoreScreenState createState() => _StoreScreenState();
 }
 
+class StoreItem {
+  String name;
+  String image;
+  int price;
+
+  StoreItem({this.name, this.image, this.price});
+}
+
+List<StoreItem> storeItems = [
+  StoreItem(
+      name: "Item 1",
+      price: 100,
+      image:
+          'https://tridacmortgages.com/wp-content/uploads/2011/09/5050_logo.png'),
+  StoreItem(
+      name: "Item 2",
+      price: 100,
+      image:
+          'https://thumbs.dreamstime.com/b/pass-text-red-grungy-vintage-round-stamp-rubber-205333260.jpg'),
+  StoreItem(
+      name: "Item 3",
+      price: 50,
+      image:
+          'https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/131654569/original/fd45e2a3d3d1643130922e98b0ce921863b6127a/make-you-cartoon-avatar.jpeg'),
+  StoreItem(
+      name: "Item 4",
+      price: 50,
+      image:
+          'https://t4.ftcdn.net/jpg/02/45/28/17/360_F_245281783_3zeOLu7mhjUmYbFlBwSNsfwQmQZzukWo.jpg'),
+  StoreItem(
+      name: "Item 5",
+      price: 50,
+      image:
+          'https://www.kindpng.com/picc/m/24-248442_female-user-avatar-woman-profile-member-user-profile.png'),
+  StoreItem(
+      name: "Item 6",
+      price: 50,
+      image:
+          'https://thumbs.dreamstime.com/b/businessman-icon-v…on-vector-male-avatar-profile-image-182095609.jpg'),
+  StoreItem(
+      name: "Item 7",
+      price: 50,
+      image:
+          'https://www.shareicon.net/data/256x256/2016/05/26/771188_man_512x512.png'),
+  StoreItem(
+      name: "Item 8",
+      price: 50,
+      image:
+          'https://stanforduni.us/wp-content/uploads/2020/05/default-avatar.png'),
+  StoreItem(
+      name: "Item 9",
+      price: 50,
+      image:
+          'https://www.shareicon.net/data/256x256/2016/05/24/770121_man_512x512.png'),
+  StoreItem(
+      name: "Item 10",
+      price: 50,
+      image: 'https://homevest.com/wp-content/uploads/2019/05/female1-512.png'),
+  StoreItem(
+      name: "Item 11",
+      price: 50,
+      image:
+          'https://www.pngitem.com/pimgs/m/0-6243_user-profile-avatar-scalable-vector-graphics-icon-woman.png'),
+];
+
 class _StoreScreenState extends State<StoreScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'STORE',
-          //style:
+        appBar: AppBar(
+          title: Text(
+            'STORE',
+            //style:
+          ),
+          backgroundColor: Color(0xFF80D8FF),
+          centerTitle: true,
+          elevation: 0.0,
+          leading: Container(),
+          actions: [
+            StoreConnector<AppState, User>(
+                builder: (context, user) {
+                  return Text("Gold: ${user.gold}");
+                },
+                converter: (store) => store.state.user),
+            IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/landing');
+                },
+                icon: Icon(Icons.home)),
+          ],
         ),
-        backgroundColor: Color(0xFF80D8FF),
-        centerTitle: true,
-        elevation: 0.0,
-        leading: Container(),
-        actions: [
-          StoreConnector<AppState, User>(
-              builder: (context, user) {
-                return Text("Gold: ${user.gold}");
+        body: Column(children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: storeItems.length,
+              itemBuilder: (BuildContext context, int index) {
+                return StoreConnector<AppState, User>(
+                    builder: (context, user) {
+                      return Container(
+                        height: 150,
+                        color: Colors.lightBlueAccent,
+                        child: Row(
+                          children: [
+                            Spacer(
+                              flex: 1,
+                            ),
+                            CircleAvatar(
+                              radius: 60,
+                              backgroundImage:
+                                  NetworkImage(storeItems[index].image),
+                            ),
+                            Spacer(
+                              flex: 2,
+                            ),
+                            Column(
+                              children: [
+                                SizedBox(height: 20),
+                                Text(
+                                  "${storeItems[index].price} Gold",
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white),
+                                ),
+                                SizedBox(height: 20),
+                                ElevatedButton.icon(
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize: Size(150,
+                                        48), // takes postional arguments as width and height
+                                  ),
+                                  onPressed: () {
+                                    if (user.gold < storeItems[index].price) {
+                                      print("${user.gold} Not enough gold!");
+                                      _showDialog("Not enough gold!",
+                                          "You have ${user.gold} gold.");
+                                    } else {
+                                      print("Bought!");
+                                      //TO DO firebase -  increase user inventory item count
+                                      //TO DO firebase -  decrease price from users gold field
+                                    }
+                                  },
+                                  icon: Icon(Icons.shopping_basket_outlined,
+                                      size: 20),
+                                  label: Text(
+                                    "BUY",
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Spacer(
+                              flex: 1,
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    converter: (store) => store.state.user);
               },
-              converter: (store) => store.state.user),
-          IconButton(
+            ),
+          )
+        ]));
+  }
+
+  void _showDialog(String title, String content) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: new Text(title),
+          content: new Text(content),
+          actions: <Widget>[
+            new OutlinedButton(
+              child: new Text("OK"),
               onPressed: () {
-                Navigator.pushNamed(context, '/landing');
+                Navigator.of(context).pop();
               },
-              icon: Icon(Icons.home)),
-        ],
-      ),
-      body: StoreListview(),
+            ),
+          ],
+        );
+      },
     );
   }
 }
